d48cf19da39704ea84dcae03b5e22750e8ad90dc
dotantai1999 <dotantai.1999@gmail.com>
2021-11-12 16:03:17 +0700

    initialize project

---
+++ EcommerceWebParent/EcommerceFrontend/src/main/java/com/shopme/site/EcommerceFronendApplication.java
@@ -0,0 +1,13 @@
+package com.shopme.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceFronendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EcommerceFronendApplication.class, args);
+  }
+
+}
